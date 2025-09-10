a69a1179398945a7c0fcbdb630eb032541547735
Wade Huber <wadehuber@gmail.com>
2025-09-10 09:11:27 -0700

    converted to fstrings

--- modules/advancedatatypes/strings.py
+++ modules/advancedatatypes/strings.py
@@ -1,27 +1,28 @@
 '''Examples of working with strings in Python'''
 
-STR1 = "Hello"
-STR2 = ['W', 'o', 'r', 'l', 'd']      # List of
-STR3 = "".join(['e', 'n', 'd', '.'])  # Create a list by joining a list of characters together
-STR4 = "This is a really long string.  Maybe too long?"
+LIST = ['W', 'o', 'r', 'l', 'd']      # List of characters
+print(f"list = {LIST} length={len(LIST)}")
 
-print("str1 = {} length={}".format(STR1, len(STR1)))
-print("str2 = {} length={}".format(STR2, len(STR2)))
-print("str3 = {} length={}".format(STR3, len(STR3)))
-print("str4 = {} length={}".format(STR4, len(STR4)))
+STR1 = "Hello"
+STR2 = "".join(['e', 'n', 'd', '.'])  # Create a list by joining a list of characters together
+STR3 = "This is a really long string.  Maybe too long?"
+
+print(f"str1 = {STR1} length={len(STR1)}")
+print(f"str2 = {STR2} length={len(STR2)}")
+print(f"str3 = {STR3} length={len(STR3)}")
 
 print("String 1 character by character: ")
 for char in STR1:
-    print("  {}".format(char))
+    print(f"  {char}")
 
-print("String 3 ASCII values: ")
+print("String 2 ASCII values: ")
 for char in STR1:
-    print("  {} (ASCII={})".format(char, ord(char)))
+    print(f"  {char} (ASCII={ord(char)})")
 
 #appending a string
 STR5 = STR1 + " " + "".join(STR2) + "!"
 print(STR5)
 
 # more string functions
-print(STR4.upper())
-print(STR4.lower())
+print(STR3.upper())
+print(STR3.lower())
